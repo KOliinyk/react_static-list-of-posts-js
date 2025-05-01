286ce716108d8447f3466994207f2d6b85ff5725
Kostya Oliinyk <aquila0905@gmail.com>
2025-05-01 22:32:59 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,7 +6,7 @@ import { CommentList } from '../CommentList/CommentList';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <div className="Post">
+  <div className="PostInfo">
     <h2 className="Post__title">{post.title}</h2>
     <p className="Post__body">{post.body}</p>
     <UserInfo user={post.user} />
